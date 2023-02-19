86bcac03e75dee4c6263752205ec6168ad38f8f5
divya vandra <divyavandra@gmail.com>
2023-02-19 00:13:09 +0000

    login page

---
+++ Password.java
@@ -0,0 +1,12 @@
+package Loginpage;
+
+// Password should always match to go further in the website
+public class Password {
+    public static void main(String[] args) {
+        System.out.println("Password should have combination of Alphabets, numbers, and special characters");
+        System.out.println("only one Alphabet capital");
+        System.out.println(" No space allowed");
+
+        //If all the criteria will match than User should be able to login without any problem
+    }
+}
---
+++ Username.java
@@ -0,0 +1,9 @@
+package Loginpage;
+// Username box should not leave empty
+public class Username {
+    public static void main(String[] args) {
+        System.out.println("User should be able to login with correct user name ");
+        System.out.println("Not more than 40 characters");
+        System.out.println("No space allowed");
+    }
+}
